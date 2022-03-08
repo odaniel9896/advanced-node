5bb60dc2b35e26aa2f9d2c17cb10dd678bd1d62f
O Daniel <soumenorgame@gmail.com>
2022-03-08 17:17:27 -0300

    move FacebookAuthenticationService to its own file

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,15 +1,5 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis';
 import { AuthenticationError } from '@/domain/errors';
-import { FacebookAuthentication } from '@/domain/features';
-
-class FacebookAuthenticationService {
-  constructor(private readonly loadFacebookUserApi: LoadFacebookUserApi) {}
-
-  async perform(params: FacebookAuthentication.Params): Promise<AuthenticationError> {
-    await this.loadFacebookUserApi.loadUser(params);
-    return new AuthenticationError();
-  }
-}
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string;
   result = undefined;
