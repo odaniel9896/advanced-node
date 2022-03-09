182fe404e5593b8ae62fe0548c236f258cde6055
O Daniel <soumenorgame@gmail.com>
2022-03-09 14:07:29 -0300

    move SUT dependencies creation to beforeAll

--- jest.config.mjs
+++ jest.config.mjs
@@ -4,10 +4,11 @@ export default {
   coverageProvider: 'babel',
   moduleNameMapper: {
     '@/tests/(.+)': '<rootDir>/tests/$1',
-    '@/(.+)': '<rootDir>/src/$1',
+    '@/(.+)': '<rootDir>/src/$1'
   },
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   transform: {
-    '\\.ts$': 'ts-jest',
+    '\\.ts$': 'ts-jest'
   },
-};
+  clearMocks: true
+}
