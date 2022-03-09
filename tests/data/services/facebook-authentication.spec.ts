0ebcf3bbc449dd8053c8659c999250fa0afb60d6
O Daniel <soumenorgame@gmail.com>
2022-03-09 10:45:42 -0300

    ensure Service update account  without overriding name

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -24,6 +24,7 @@ describe('FacebookAuthenticationService ', () => {
       facebookId: 'any_facebook_id'
     })
     userAccountRepo = mock()
+    userAccountRepo.load.mockResolvedValue(undefined)
     sut = new FacebookAuthenticationService(facebookApi, userAccountRepo)
   })
 
@@ -50,7 +51,6 @@ describe('FacebookAuthenticationService ', () => {
   })
 
   it('should call CreateFacebookAccountRepo when LoadUserAccountRepository returns undefined', async () => {
-    userAccountRepo.load.mockResolvedValueOnce(undefined)
     await sut.perform({ token })
 
     expect(userAccountRepo.createFromFacebook).toHaveBeenCalledWith({
@@ -71,6 +71,20 @@ describe('FacebookAuthenticationService ', () => {
     expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledWith({
       id: 'any_id',
       name: 'any_name',
+      facebookId: 'any_facebook_id'
+    })
+    expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledTimes(1)
+  })
+
+  it('should update account name', async () => {
+    userAccountRepo.load.mockResolvedValueOnce({
+      id: 'any_id'
+    })
+    await sut.perform({ token })
+
+    expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledWith({
+      id: 'any_id',
+      name: 'any_facebook_name',
       facebookId: 'any_facebook_id'
     })
     expect(userAccountRepo.updateWithFacebook).toHaveBeenCalledTimes(1)
