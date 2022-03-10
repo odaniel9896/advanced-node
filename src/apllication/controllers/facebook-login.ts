3b85027088a172be44354e94ddefcfe686e86857
O Daniel <soumenorgame@gmail.com>
2022-03-10 11:56:32 -0300

    ensure use generic instead of any

--- src/apllication/controllers/facebook-login.ts
+++ src/apllication/controllers/facebook-login.ts
@@ -3,18 +3,19 @@ import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
 import { RequiredFieldError } from '@/apllication/errors'
 
+type HttpRequest = {
+  token: string | null | undefined
+}
+type Model = Error | {
+  accessToken: string
+}
 export class FacebookLoginController {
   constructor(private readonly facebookAuthentication: FacebookAuthentication) {}
-  async handle(httpRequest: any): Promise<HttpResponse> {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
     try {
       if (!httpRequest.token) return badRequest(new RequiredFieldError('token'))
-
-      const accessToken = await this.facebookAuthentication.perform({
-        token: httpRequest.token
-      })
-      if (accessToken instanceof AccessToken) {
-        return ok({ accessToken: accessToken.value })
-      }
+      const accessToken = await this.facebookAuthentication.perform({ token: httpRequest.token })
+      if (accessToken instanceof AccessToken) return ok({ accessToken: accessToken.value })
       return unauthorized()
     } catch (error) {
       return serverError(error)
