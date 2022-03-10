666dad8298c5a244d8bf5802d70d05ae7a9229bc
O Daniel <soumenorgame@gmail.com>
2022-03-10 09:24:17 -0300

    ensure PGUserAccountRepository returns undefined if email exists

--- tests/infra/postress/repos/user-account.spec.ts
+++ tests/infra/postress/repos/user-account.spec.ts
@@ -39,14 +39,28 @@ describe('PgUserAccountRepository', () => {
         entities: [PGUser]
       })
       await connection.synchronize()
-      console.log('salve')
       const pgUserRepository = getRepository(PGUser)
       await pgUserRepository.save({ email: 'existing_email' })
       const sut = new PGUserAccountRepository()
 
       const account = await sut.load({ email: 'existing_email' })
-      console.log(account, 'sou account')
       expect(account).toEqual({ id: '1' })
+      await connection.close()
+    })
+
+    it('should return undefined id email not exists', async () => {
+      const db = newDb()
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PGUser]
+      })
+      await connection.synchronize()
+      const sut = new PGUserAccountRepository()
+
+      const account = await sut.load({ email: 'new_email' })
+
+      expect(account).toBe(undefined)
+      await connection.close()
     })
   })
 })
