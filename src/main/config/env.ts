6a1266a55b9b159af4a42d64697951ad9ad4a657
Daniel <soumenorgame@gmail.com>
2022-03-30 10:43:56 -0300

    add value in gitignore

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,7 @@
 export const env = {
   facebookApi: {
-    clientId: process.env.FB_CLIENT_ID ?? '656931728917300',
-    clientSecret: process.env.FB_CLIENT_SECRET ?? '8c95603afb29fb58bfe854d61df93c85'
+    clientId: process.env.FB_CLIENT_ID ?? '',
+    clientSecret: process.env.FB_CLIENT_SECRET ?? ''
   },
   port: process.env.PORT ?? 3333,
   jwtSecret: process.env.JWT_SECRET ?? 'djasdsajdasjidji'
